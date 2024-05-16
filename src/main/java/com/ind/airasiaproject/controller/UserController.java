4124b3c1ee1efa6253474b1e95b28ff800e26ef5
Sachin007060 <sachinbaskandi20@gmail.com>
2024-05-16 16:12:03 +0530

    create api of user controller of user login

--- src/main/java/com/ind/airasiaproject/controller/UserController.java
+++ src/main/java/com/ind/airasiaproject/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.ind.airasiaproject.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +22,10 @@ public class UserController {
 	@PostMapping("/saveUserDetails")
 	public ApplicationResponse<User> userRegisterService(@RequestBody User user){
 		return userService.userRegisterService(user);
+	}
+	
+	@GetMapping("/userLogin/{email}/{password}")
+	public ApplicationResponse<User> fetchUserByEmailForLoginService(@PathVariable String email,@PathVariable String password){
+		return userService.fetchUserByEmailForLoginService(email, password);
 	}
 }
