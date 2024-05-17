9faf13b44910df1ab5e0cd0274f628d1af2aa112
Sachin007060 <sachinbaskandi20@gmail.com>
2024-05-17 23:34:17 +0530

    create a new abstract method inside flight repository class find by email

--- src/main/java/com/ind/airasiaproject/repository/FlightRepository.java
+++ src/main/java/com/ind/airasiaproject/repository/FlightRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ind.airasiaproject.dto.Flight;
 
 public interface FlightRepository extends JpaRepository<Flight, String>{
-
+	
+	public Flight findByFlightNumber(String flightNumber);
+	
 }
